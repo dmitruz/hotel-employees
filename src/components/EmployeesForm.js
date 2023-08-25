778c7e0f6531d1e8910a99e863cca645ad98f2b0
dmitruz <dmitruz970@gmail.com>
2023-08-25 20:33:58 +0300

    created employees list

--- src/components/EmployeesForm.js
+++ src/components/EmployeesForm.js
@@ -1,6 +1,32 @@
 import './EmployeesForm.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName, changeNumber, addEmployee } from '../store';
 
 const EmployeesForm = () => {
+  const dispatch = useDispatch();
+  const { name, number } = useSelector(state => {
+    return {
+      name: state.form.name,
+      number: state.form.number,
+    };
+  });
+
+  const handleNameChange = e => {
+    dispatch(changeName(e.target.value));
+  };
+
+  const handleNumberChange = e => {
+    const employeeNumber = parseInt(e.target.value) || 0;
+    dispatch(changeNumber(employeeNumber));
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(addEmployee({ name, number }));
+    dispatch(changeName(''));
+    dispatch(changeNumber(0));
+  };
+
   return (
     <>
       <div className="employees__header">
@@ -8,15 +34,20 @@ const EmployeesForm = () => {
       </div>
       <div className="form__panel">
         <h3 className="subtitle">Add Employee</h3>
-        <form className="employees__form">
+        <form onSubmit={handleSubmit} className="employees__form">
           <div className="field__group">
             <div className="field">
               <label className="label">Name</label>
-              <input className="input is-expanded" />
+              <input className="input is-expanded" value={name} onChange={handleNameChange} />
             </div>
             <div className="field">
               <label className="label">Phone</label>
-              <input className="input is-expanded" type="number" />
+              <input
+                className="input is-expanded"
+                value={number}
+                onChange={handleNumberChange}
+                type="number"
+              />
             </div>
           </div>
           <div className="form__submit-btn">
--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -1,5 +1,44 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { removeEmployee } from '../store';
+
 const EmployeesList = () => {
-  return <div>Employees List</div>;
+  const dispatch = useDispatch();
+  const { employees, name } = useSelector(({ form, employees: { data, searchTerm } }) => {
+    const filteredCars = data.filter(employee =>
+      employee.name.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+
+    return {
+      employees: filteredCars,
+      name: form.name,
+    };
+  });
+
+  const handleEmployeeDelete = employee => {
+    dispatch(removeEmployee(employee.id));
+  };
+
+  const renderedEmployees = employees.map(employee => {
+    const bold = name && employee.name.toLowerCase().includes(name.toLowerCase());
+
+    return (
+      <div key={employee.id} className={`panel ${bold && 'bold'}`}>
+        <p>
+          {employee.name} - ${employee.cost}
+        </p>
+        <button className="button is-danger" onClick={() => handleEmployeeDelete(employee)}>
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="car-list">
+      {renderedEmployees}
+      <hr />
+    </div>
+  );
 };
 
 export default EmployeesList;
