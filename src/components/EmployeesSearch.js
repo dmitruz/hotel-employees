bb366a403f32c4d847020f354ff0d8753184e4a8
dmitruz <dmitruz970@gmail.com>
2023-08-28 20:05:53 +0300

    fix searchterm

--- src/components/EmployeesSearch.js
+++ src/components/EmployeesSearch.js
@@ -1,12 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { changeSearchTerm } from '../store';
 import './EmployeesSearch.scss';
 import { RiUserSearchFill } from 'react-icons/ri';
+
 const EmployeesSearch = () => {
+  const dispatch = useDispatch();
+  const searchTerm = useSelector(state => {
+    return state.employees.searchTerm;
+  });
+
+  const handleSearchTermChange = e => {
+    dispatch(changeSearchTerm(e.target.value));
+  };
   return (
     <div className="list__header">
       <h3 className="title">ALL Employees</h3>
       <div className="search">
         <RiUserSearchFill />
-        <input className="input" placeholder="Search employee" />
+        <input
+          className="input"
+          placeholder="Search employee"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        />
       </div>
     </div>
   );
